f9df556a42002542c66d45a734d317365491fe6e
James Morales Moreno <james.moralesm@perficient.com>
2021-07-01 09:05:15 -0500

    Gerkhin syntax modification

--- test/buy-tshirt.spec.ts
+++ test/buy-tshirt.spec.ts
@@ -5,12 +5,12 @@ import {
   BankPaymentPage, OrderSummaryPage,
 } from '../src/page';
 
-describe('Go to a shopping page', () => {
+describe('Given a shopping page', () => {
   beforeAll(async () => {
     await browser.waitForAngularEnabled(false);
     await browser.get('http://automationpractice.com/');
   });
-  describe('Buy a t-shirt process', () => {
+  describe('When buy a t-shirt process', () => {
     beforeAll(async () => {
       const menuContentPage: MenuContentPage = new MenuContentPage();
       const productListPage: ProductListPage = new ProductListPage();
@@ -22,26 +22,26 @@ describe('Go to a shopping page', () => {
       await productAddedModalPage.goToSummaryMenu();
       await summaryStepPage.goToSignInMenu();
     });
-    describe('Login process', () => {
+    describe('And login process', () => {
       beforeAll(async () => {
         const signInStepPage: SignInStepPage = new SignInStepPage();
         await signInStepPage.login('aperdomobo@gmail.com', 'WorkshopProtractor');
       });
-      describe('Choosing address and shipping', () => {
+      describe('And choosing address and shipping', () => {
         beforeAll(async () => {
           const addressStepPage: AddressStepPage = new AddressStepPage();
           const shippingStepPage: ShippingStepPage = new ShippingStepPage();
           await addressStepPage.goToShippingStepMenu();
           await shippingStepPage.goToPaymentMenu();
         });
-        describe('Bank Payment', () => {
+        describe('And Bank Payment', () => {
           beforeAll(async () => {
             const paymentStepPage: PaymentStepPage = new PaymentStepPage();
             const bankPaymentPage: BankPaymentPage = new BankPaymentPage();
             await paymentStepPage.goToBankPaymentMenu();
             await bankPaymentPage.goToOrderSummaryMenu();
           });
-          it('then should be bought a t-shirt', async () => {
+          it('Then should be bought a t-shirt', async () => {
             const orderSummaryPage: OrderSummaryPage = new OrderSummaryPage();
             await expect(orderSummaryPage.goToProcessComplete()).toBe('Your order on My Store is complete.');
           });
--- test/i-frame.spec.ts
+++ test/i-frame.spec.ts
@@ -3,39 +3,39 @@ import { IFramePage } from '../src/page';
 
 describe('Given a SDET learning protractor', () => {
   const iFrame: IFramePage = new IFramePage();
-  describe('and then open Iframe page', () => {
+  describe('When open Iframe page', () => {
     beforeEach(async () => {
       await browser.waitForAngularEnabled(false);
       await browser.driver.get('https://demoqa.com/frames');
     });
 
-    it('and then should have as title frames', async () => {
+    it('Then should have as title frames', async () => {
       expect(await iFrame.getTittlePage()).toBe('Frames');
     });
-    describe('and then set frame height', () => {
+    describe('And set frame height', () => {
       const newIframeHeight = 500;
       beforeEach(async () => {
         await iFrame.setFormFrameHeight(newIframeHeight);
       });
 
-      it('and then should have a different height than before', async () => {
+      it('Then should have a different height than before', async () => {
         expect(await iFrame.getFrameHeight()).toBe(newIframeHeight);
       });
-      describe('and then switch to frame 1', () => {
+      describe('And switch to frame 1', () => {
         beforeEach(async () => {
           await iFrame.switchToFrame();
         });
 
-        it('and then should have a different title', async () => {
+        it('Then should have a different title', async () => {
           expect(await iFrame.getTittlePage()).toBe('This is a sample page');
         });
       });
-      describe('and then switch to main context', () => {
+      describe('When switch to main context', () => {
         beforeEach(async () => {
           await iFrame.switchToMainPage();
         });
 
-        it('and then should have a different title', async () => {
+        it('Then should have a different title', async () => {
           expect(await iFrame.getTittlePage()).toBe('Frames');
         });
       });
