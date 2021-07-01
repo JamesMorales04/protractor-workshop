0e947a9fd88a850358c722282fff1996c7babc30
James Morales Moreno <james.moralesm@perficient.com>
2021-07-01 08:22:14 -0500

    small semantic fix

--- test/i-frame.spec.ts
+++ test/i-frame.spec.ts
@@ -3,39 +3,39 @@ import { IFramePage } from '../src/page';
 
 describe('Given a SDET learning protractor', () => {
   const iFrame: IFramePage = new IFramePage();
-  describe('then open Iframe page', () => {
+  describe('and then open Iframe page', () => {
     beforeEach(async () => {
       await browser.waitForAngularEnabled(false);
       await browser.driver.get('https://demoqa.com/frames');
     });
 
-    it('then should have as title frames', async () => {
+    it('and then should have as title frames', async () => {
       expect(await iFrame.getTittlePage()).toBe('Frames');
     });
-    describe('Then set frame height', () => {
+    describe('and then set frame height', () => {
       const newIframeHeight = 500;
       beforeEach(async () => {
         await iFrame.setFormFrameHeight(newIframeHeight);
       });
 
-      it('then should have a different height than before', async () => {
+      it('and then should have a different height than before', async () => {
         expect(await iFrame.getFrameHeight()).toBe(newIframeHeight);
       });
-      describe('Then switch to frame 1', () => {
+      describe('and then switch to frame 1', () => {
         beforeEach(async () => {
           await iFrame.switchToFrame();
         });
 
-        it('then should have a different title', async () => {
+        it('and then should have a different title', async () => {
           expect(await iFrame.getTittlePage()).toBe('This is a sample page');
         });
       });
-      describe('Then switch to main context', () => {
+      describe('and then switch to main context', () => {
         beforeEach(async () => {
           await iFrame.switchToMainPage();
         });
 
-        it('then should have a different title', async () => {
+        it('and then should have a different title', async () => {
           expect(await iFrame.getTittlePage()).toBe('Frames');
         });
       });
